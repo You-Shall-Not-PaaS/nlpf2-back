a0b3f5beb8b245c9ece36619088094400111835e
zakaria.ben-allal <zakaria.ben-allal@epita.fr>
2021-11-13 00:26:53 +0100

    Add town average price feature

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 var cors = require('cors')
 
-const {get_paginated_property, filter_properties} = require('./propertyService');
+const { get_paginated_property, filter_properties, get_average_price } = require('./propertyService');
 
 
 const app = express();
 
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
 
 
@@ -20,12 +20,14 @@ app.listen(PORT, () => {
 
 //debug route
 app.get('/', (req, res) => {
-    res.send("hello NLPF");
+  res.send("hello NLPF");
 });
 
 app.get('/properties/:page/', get_paginated_property);
 
 app.get('/properties-filter/:page/', filter_properties)
+
+app.get('/properties/town/average-price/:id', get_average_price)
 
 
 module.exports = app
